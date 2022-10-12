36dfe08e48da9cf617e23728888abffd8957bfa6
Shubham Kumar <shubham.kumar18@vit.edu>
2022-10-12 17:23:01 +0530

    adding diff.cpp

---
+++ diff.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int diff(int a, int b){
+    return a-b;
+}
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<"Diff of "<<a<<" and "<<b<<" is "<<diff(a,b);
+    return 0;
+}
